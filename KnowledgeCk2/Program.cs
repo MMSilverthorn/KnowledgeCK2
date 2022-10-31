ad591d41253c2f917084f024fe3d660370736752
Mary Silverthorn <msilverthorn@gmail.com>
2022-10-31 19:24:56 -0400

    Finally finished and it actually works

---
+++ KnowledgeCk2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace KnowledgeCk2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many books do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+
+
+            var recordList = new List<myBooks>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var entry = new myBooks();
+
+                Console.WriteLine("Enter your the authors name: ");
+                entry.AuthorName = Console.ReadLine();
+
+                Console.WriteLine("Enter the book title: ");
+                entry.Title = Console.ReadLine();
+
+                recordList.Add(new myBooks() { AuthorName = entry.AuthorName, Title = entry.Title });
+            }
+
+
+
+            // Print out the list of records using Console.WriteLine()
+            foreach (var entry in recordList)
+                Console.WriteLine("Author: " + entry.AuthorName + "\nTitle: " + entry.Title + "\n");
+
+        }
+    }
+}
---
+++ KnowledgeCk2/myBooks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCk2
+
+{
+    internal class myBooks : Author
+    {
+        public string Title { get; set; }
+    }
+}
